edbadb1c706f47d53ac65d9fbb18d670f48e52eb
muhammad-azhar-dev <muhammadazhar9920@gmail.com>
2026-03-16 02:30:46 +0500

    Database Connection

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
+from src.utils.db import Base, engine
 
-app = FastAPI()
+try:
+    Base.metadata.create_all(engine)
+    print("-------------- Database Connected ----------------")
+except Exception as e:
+    print("-------------- Database Connection ERROR ----------------")
+
+app = FastAPI(title="This is my Task Management Application")
+
