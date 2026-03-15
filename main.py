ee0eb82f42e38d9201dc285b5e4e87edc672dd1c
muhammad-azhar-dev <muhammadazhar9920@gmail.com>
2026-03-16 03:10:24 +0500

    Created Task POST Route to add tasks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from src.utils.db import Base, engine
-from src.tasks.model import TaskModel
+from src.tasks.router import task_routes
 
 try:
     Base.metadata.create_all(engine)
@@ -9,4 +9,5 @@ except Exception as e:
     print("-------------- Database Connection ERROR ----------------")
 
 app = FastAPI(title="This is my Task Management Application")
+app.include_router(task_routes)
 
