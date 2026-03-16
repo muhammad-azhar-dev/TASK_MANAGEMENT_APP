1d9dc12eb359d3b506f255692f01bbbfd99f57ee
muhammad-azhar-dev <muhammadazhar9920@gmail.com>
2026-03-17 00:42:01 +0500

    Created Two GET Routes for getting tasks

--- src/tasks/controller.py
+++ src/tasks/controller.py
@@ -1,6 +1,7 @@
 from src.tasks.dtos import TaskSchema
 from sqlalchemy.orm import Session
 from src.tasks.model import TaskModel
+from fastapi import HTTPException
 
 def create_task(body:TaskSchema, db:Session):
     data = body.model_dump()
@@ -13,3 +14,13 @@ def create_task(body:TaskSchema, db:Session):
     db.commit()
     db.refresh(new_task)
     return {"status":"Task Created Successfully", "data":new_task}
+
+def get_tasks(db:Session):
+    tasks = db.query(TaskModel).all()
+    return {"status":"Tasks Fetched Successfully", "data":tasks}
+
+def get_one_task(task_id:int, db:Session):
+    task = db.query(TaskModel).get(task_id)
+    if not task:
+        raise HTTPException(404, detail="task_id is Incorrect!")
+    return {"status":"Task Fetched Successfully", "data":task}
--- src/tasks/router.py
+++ src/tasks/router.py
@@ -8,3 +8,11 @@ task_routes = APIRouter(prefix="/tasks")
 @task_routes.post("/create")
 def create_task(body:TaskSchema, db = Depends(get_db)):
     return controller.create_task(body, db)
+
+@task_routes.get("/all_tasks")
+def get_tasks(db = Depends(get_db)):
+    return controller.get_tasks(db)
+
+@task_routes.get("/one_task/{task_id}")
+def get_one_task(task_id:int, db=Depends(get_db)):
+    return controller.get_one_task(task_id, db)
